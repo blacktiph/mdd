c5d859f109fbdb52e2359815d93cefb22f01e996
blacktiph <blacktiph@fullsail.edu>
2013-12-06 19:04:54 -0500

    [delivers #62031106] leaderboard code

--- code/js/leaderboard.controller.js
+++ code/js/leaderboard.controller.js
@@ -48,31 +48,3 @@ btcApp.controller("leaderboard", ["$scope", "$firebase", function($scope, $fireb
   	angularFireAuth.initialize(new Firebase(FBURL), {scope: $rootScope, name: 'auth', path: '/signin'});
  	$rootScope.FBURL = FBURL;
 }]);
-
-// var controllers = {};
-
-// controllers.leaderboard = function($scope){
-// 	$scope.anglers = [
-// 		{name:'John Smith', species:'blacktip', overallLength:78.89, forkLength:67.4, girth:42, weight: 110},
-//       	{name:'Rob Stuart', species:'blacktip', overallLength:72, forkLength:62, girth:28, weight: 190},
-//        	{name:'Adam Larson', species:'blacktip', overallLength:74, forkLength:64, girth:35, weight: 160},
-//        	{name:'Ruby Sue', species:'blacktip', overallLength:65, forkLength:55, girth:26, weight: 60},
-//        	{name:'Pete Duck', species:'blacktip', overallLength:79, forkLength:71, girth:46, weight: 90}
-// 	];
-
-// 	$scope.orderByWeight = "-weight";
-
-// 	$scope.newEntry = {};
-
-// 	//Set default value of dropdown list
-// 	$scope.newEntry.species = "blacknose";
-
-// 	$scope.addEntry = function(){
-// 		$scope.newEntry.weight = ($scope.newEntry.girth * $scope.newEntry.girth * $scope.newEntry.forkLength)/800;
-// 		$scope.anglers.push($scope.newEntry);
-// 		console.log('$scope.newEntry',$scope.newEntry, $scope.anglers);
-
-// 	};
-// };
-
-// btcApp.controller(controllers);
